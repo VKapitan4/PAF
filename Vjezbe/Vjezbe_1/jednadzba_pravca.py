2470410826f0e43ac42b6a9f13477ff153869bef
VKapitan4 <vkapitano@pmfst.hr>
2022-03-05 21:43:22 +0100

    Gotov treci zadatak

--- Vjezbe/Vjezbe_1/jednadzba_pravca.py
+++ Vjezbe/Vjezbe_1/jednadzba_pravca.py
@@ -1,9 +1,48 @@
-x1 = float(input("Upisite prvu x koordinatu:"))
-y1 = float(input("Upisite prvu y koordinatu:"))
-x2 = float(input("Upisite drugu x koordinatu:"))
-y2 = float(input("Upisite drugu y koordinatu:"))
+def check_float(potential_float):
+    try:
+        float(potential_float)
+        return True
+    except ValueError:
+        return False
 
-k=(y2-y1)/(x2-x1)
-l = y1 - k*x1
+test=False
+while test==False:
+    a = input("Upisite prvu x koordinatu:")
+    test = check_float(a)
+    if test==False:
+        print("Pogreška pri unosu. Ponovite unos.")
+x1 = float(a)
 
-print(f"Jednadzba pravca: y={k}x+{l}")
+test=False
+while test==False:
+    a = input("Upisite prvu y koordinatu:")
+    test = check_float(a)
+    if test==False:
+        print("Pogreška pri unosu. Ponovite unos.")
+y1 = float(a)
+
+test=False
+while test==False:
+    a = input("Upisite drugu x koordinatu:")
+    test = check_float(a)
+    if test==False:
+        print("Pogreška pri unosu. Ponovite unos.")
+x2 = float(a)
+
+test=False
+while test==False:
+    a = input("Upisite drugu y koordinatu:")
+    test = check_float(a)
+    if test==False:
+        print("Pogreška pri unosu. Ponovite unos.")
+y2 = float(a)
+
+try:
+    k=(y2-y1)/(x2-x1)
+    l = y1 - k*x1
+    if l<0:
+        print(f"Jednadzba pravca: y={k}x-{abs(l)}")
+    else:
+        print(f"Jednadzba pravca: y={k}x+{l}")
+except:
+    print(f"Jednadzba pravca: x={x1}")
