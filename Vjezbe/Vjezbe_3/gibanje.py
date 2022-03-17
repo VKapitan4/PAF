c57e0aa6f38b2f07e5897d9b28a229bdd31c1d7e
VKapitan4 <vkapitano@pmfst.hr>
2022-03-17 20:54:34 +0100

    vjezbe 3

--- Vjezbe/Vjezbe_3/gibanje.py
+++ Vjezbe/Vjezbe_3/gibanje.py
@@ -8,3 +8,6 @@ p1.reset()
 p1.set_initial_conditions(5, 30, 30, 50)
 print(p1.range())
 p1.plot_trajectory()
+domet = p1.range()
+razlika=domet-2.2069964418563676013346665921329
+print(f'domet se razlikuje za: {razlika} ' )
