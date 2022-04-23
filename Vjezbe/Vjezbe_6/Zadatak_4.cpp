c23c6ea3856f373a1b1d5ccf845739a2936effcd
VKapitan4 <vkapitano@pmfst.hr>
2022-04-23 23:23:39 +0200

    sustav jednadzbi

--- Vjezbe/Vjezbe_6/Zadatak_4.cpp
+++ Vjezbe/Vjezbe_6/Zadatak_4.cpp
@@ -1,13 +1,26 @@
 #include <iostream>
 
 void rjesavanje_sustava(float a1, float b1, float c1, float a2, float b2, float c2){
-    float x = c1 - c2*(b2/b1) + a2*(b2/b1);
-    float y = (c1 - a1*x)/b1;
-    std::cout <<"x="<<x<< std::endl;
-    std::cout <<"y="<<y<< std::endl;
+    if (a1*b2-b1*a2!=0){
+        if (b2!=0){
+            float x = (c1-c2*(b1/b2))/(a1-a2*(b1/b2));
+            float y = (c2 - a2*x)/b2;
+            std::cout <<"x="<<x<< std::endl;
+            std::cout <<"y="<<y<< std::endl;
+        }
+        else{
+            float x = c2/a2;
+            float y = (c1 - a1*x)/b1;
+            std::cout <<"x="<<x<< std::endl;
+            std::cout <<"y="<<y<< std::endl;
+        }
+    }
+    else{
+        std::cout <<"Rjesenje nije jedinstveno ili ne postoji."<< std::endl;
+    }
 }
 
 int main(){
-    rjesavanje_sustava(1,5,2,3,6,5);
+    rjesavanje_sustava(1,1,0,1,1,2);
     return 0;
 }
