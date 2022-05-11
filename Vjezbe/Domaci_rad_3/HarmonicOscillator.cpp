4933fbf89236f83dc24e35133e16920fbd4c5ec2
VKapitan4 <vkapitano@pmfst.hr>
2022-05-11 22:20:02 +0200

    crtanje iz txt

--- Vjezbe/Domaci_rad_3/HarmonicOscillator.cpp
+++ Vjezbe/Domaci_rad_3/HarmonicOscillator.cpp
@@ -22,6 +22,13 @@ HarmonicOscillator::~HarmonicOscillator(){
 
 }
 
+void HarmonicOscillator::restart(){
+    _t = 0;
+    _x = _x0;
+    _v = _v0;
+    _a = -(_k/_m)*_x0;
+}
+
 void HarmonicOscillator::move(){
     _t+=_dt;
     _a = -(_k/_m)*_x;
@@ -35,13 +42,21 @@ void HarmonicOscillator::export_data(){
         fw1 << _t << "\n";
         ofstream fw2("/Users/test/Documents/Programi/PAF/Vjezbe/Domaci_rad_3/Oscillator_x.txt", ofstream::out);
         fw2 << _x <<"\n";
+        ofstream fw3("/Users/test/Documents/Programi/PAF/Vjezbe/Domaci_rad_3/Oscillator_v.txt", ofstream::out);
+        fw3 << _v <<"\n";
+        ofstream fw4("/Users/test/Documents/Programi/PAF/Vjezbe/Domaci_rad_3/Oscillator_a.txt", ofstream::out);
+        fw4 << _a <<"\n";
         while(_t < _vrijeme){
             move();
             fw1 << _t << "\n";
             fw2 << _x << "\n";
+            fw3 << _v << "\n";
+            fw4 << _a << "\n";
         }
         fw1.close();
         fw2.close();
+        fw3.close();
+        fw4.close();
     }
     else{
         cout << "Pozovite funkciju restart." <<  endl;
--- Vjezbe/Domaci_rad_3/crtanje.py
+++ Vjezbe/Domaci_rad_3/crtanje.py
@@ -3,17 +3,26 @@ import numpy as np
 
 t = []
 x = []
+v = []
+a = []
 
 my_file_t = open("Oscillator_t.txt", "r")
 my_file_x = open("Oscillator_x.txt", "r")
+my_file_v = open("Oscillator_v.txt", "r")
+my_file_a = open("Oscillator_a.txt", "r")
 t_strings = my_file_t.readlines()
 x_strings = my_file_x.readlines()
-t=[]
-x=[]
+v_strings = my_file_v.readlines()
+a_strings = my_file_a.readlines()
+
 for i in t_strings:
     t.append(float(i))
 for i in x_strings:
     x.append(float(i))
+for i in v_strings:
+    v.append(float(i))
+for i in a_strings:
+    a.append(float(i))
 
 def fun(t):
     return 5*np.cos(np.sqrt(5/10)*t)+(2/np.sqrt(5/10))*np.sin(np.sqrt(5/10)*t)
@@ -22,10 +31,30 @@ def fun(t):
 #for i in t:
 #    x_analiticki.append(fun(i))
 
-plt.figure(figsize=(12,3))
+#plt.figure(figsize=(12,3))
 #plt.plot(t, x_analiticki, zorder=0, color='blue')
-plt.scatter(t, x, s=5, color='red')
+#plt.scatter(t, x, s=5, color='red')
+#plt.xlabel("$t(s)$")
+#plt.ylabel("$x(m)$")
+#plt.tight_layout()
+#plt.show()
+
+plt.figure(figsize=(10,7))
+
+plt.subplot(3,1,1)
+plt.scatter(t, x, s=2)
 plt.xlabel("$t(s)$")
 plt.ylabel("$x(m)$")
-#plt.tight_layout()
+
+plt.subplot(3,1,2)
+plt.scatter(t, v, s=2)
+plt.xlabel("$t(s)$")
+plt.ylabel("$v(m)$")
+
+plt.subplot(3,1,3)
+plt.scatter(t, a, s=2)
+plt.xlabel("$t(s)$")
+plt.ylabel("$a(m)$")
+
+plt.tight_layout()
 plt.show()
