002b69c5785f198bca970b18ec5d19ad38035e21
VKapitan4 <vkapitano@pmfst.hr>
2022-05-27 20:48:09 +0200

    pozitron

--- Vjezbe/Vjezbe_10/Zadatak_2.py
+++ Vjezbe/Vjezbe_10/Zadatak_2.py
@@ -10,8 +10,8 @@ y1 = p1.y
 z1 = p1.z
 p1.reset()
 
-p1.set_initial_conditions(1, -1, [0,0,0], [0,0,1], [0,0,0], [0.1,0.1,0.1], 0.01, 25)
-p1.evolve('r')
+p1.set_initial_conditions(1, 1, [0,0,0], [0,0,1], [0,0,0], [0.1,0.1,0.1], 0.01, 25)
+p1.evolve()
 x2 = p1.x
 y2 = p1.y
 z2 = p1.z
@@ -19,8 +19,8 @@ p1.reset()
 
 fig = plt.figure()
 ax = fig.add_subplot(projection='3d')
-ax.plot(x1, y1, z1, label='elektron')
-ax.plot(x2, y2, z2, label='pozitron')
+ax.plot(x1, y1, z1, label='Runge-Kutta')
+ax.plot(x2, y2, z2, label='Euler')
 ax.set_xlabel('$x(m)$')
 ax.set_ylabel('$y(m)$')
 ax.set_zlabel('$z(m)$')
